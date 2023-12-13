d67dc08c74153544a514c4dd388c82f3cedc928d
akshaya kini <akshaya.kini12@gmail.com>
2023-12-13 10:14:22 +0100

    Add feature to create orders and make payment for unauthenticated users

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,6 @@ const Order = require("../models/order.js");
 const Product = require("../models/product.js");
 
 const addOrderItem = async (req, res) => {
-  console.log(req.body);
   const {
     orderItems,
     shippingAddress,
@@ -17,6 +16,35 @@ const addOrderItem = async (req, res) => {
     const order = new Order({
       ...req.body,
       user: req.user._id,
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item.product._id,
+        _id: undefined,
+      })),
+    });
+    const createdOrder = await order.save();
+    return res.status(201).json(createdOrder);
+  } else {
+    res.status(400).json({ message: "No order items." });
+  }
+};
+
+const addOrderItemUnlogged = async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    email,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length > 0) {
+    const order = new Order({
+      ...req.body,
+      guestUser: email,
       orderItems: orderItems.map((item) => ({
         ...item,
         product: item.product._id,
@@ -48,23 +76,21 @@ const getOrder = async (req, res) => {
   }
 };
 
-/**
- * 
- * const logoutHandler = async () => {
-    try {
-      await logoutApiCall().unwrap();
-      dispatch(logout());
-      navigate('/login');
-      toast.success('Your session has expired.  Please login again', {});
-    } catch (err) {
-      console.log(err);
+const getOrderUnlogged = async (req, res) => {
+  try {
+    const order = await Order.findById(req.params.id).populate({
+      path: "orderItems.product",
+      model: Product,
+    });
+    if (order) {
+      return res.json(order);
+    } else {
+      res.status(404).json({ message: "Order not found." });
     }
-  };
- 
-  if (error && error?.data?.message === 'Not authorized - no token') {
-    logoutHandler();
+  } catch (error) {
+    console.log(error);
   }
- */
+};
 
 const updateOrderPayment = async (req, res) => {
   try {
@@ -87,4 +113,10 @@ const updateOrderPayment = async (req, res) => {
   }
 };
 
-module.exports = { addOrderItem, getOrder, updateOrderPayment };
+module.exports = {
+  addOrderItem,
+  addOrderItemUnlogged,
+  getOrder,
+  getOrderUnlogged,
+  updateOrderPayment,
+};
--- routes/order.js
+++ routes/order.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const {
   addOrderItem,
+  addOrderItemUnlogged,
   getOrder,
-
+  getOrderUnlogged,
   updateOrderPayment,
 } = require("../controllers/order");
 const { auth } = require("../middleware/auth");
 const router = new express.Router();
 
 router.post("/order/create", auth, addOrderItem);
+router.post("/order/create-unlogged", addOrderItemUnlogged);
 router.get("/order/:id", auth, getOrder);
+router.get("/order-unlogged/:id", getOrderUnlogged);
 // router.post("/order/checkout", checkout);
 router.put("/order/:id/pay", updateOrderPayment);
 
