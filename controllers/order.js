066bc8842893330cbd4b7b78aa1488fe14e142a6
akshkin <akshaya.kini12@gmail.com>
2024-08-28 11:46:37 +0200

    Add route and controller to get all orders

--- controllers/order.js
+++ controllers/order.js
@@ -1,4 +1,5 @@
 const Order = require("../models/order.js");
+const Orders = require("../models/orders.js");
 const Product = require("../models/product.js");
 
 const addOrderItem = async (req, res) => {
@@ -113,10 +114,49 @@ const updateOrderPayment = async (req, res) => {
   }
 };
 
+const getOrders = async (req, res) => {
+  const userId = req.user.id;
+  const { page = 1 } = req.query;
+  const limit = 8;
+
+  const skipAmount = (page - 1) * limit;
+
+  try {
+    let orders = await Order.find({ user: userId })
+      .populate({
+        path: "orderItems",
+        populate: {
+          path: "product",
+          model: Product,
+        },
+      })
+      .limit(limit)
+      .skip(skipAmount)
+      .sort({ createdAt: -1 });
+
+    const totalOrders = await Order.countDocuments({ user: userId });
+
+    const isNextPage = totalOrders > skipAmount + orders.length;
+
+    if (!orders) {
+      return res.json({ message: "No orders found." });
+    }
+
+    res.status(200).json({
+      orders,
+      isNextPage,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Could not get orders" });
+  }
+};
+
 module.exports = {
   addOrderItem,
   addOrderItemUnlogged,
   getOrder,
   getOrderUnlogged,
   updateOrderPayment,
+  getOrders,
 };
